b565398e36b8ef158a38cf4dc556d158234940a2
pi <josueibecerra@gmail.com>
2019-06-10 15:41:24 +0100

    working on displaying weight

--- Weight_Sensors_Module.py
+++ Weight_Sensors_Module.py
@@ -1,3 +1,10 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO  # import GPIO
+from hx711 import HX711  # import the class HX711
 
 
+def Weight:
+    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+    hx = HX711(dout_pin=21, pd_sck_pin=20)  # create an object
+    print(hx.get_raw_data_mean())  # get raw data reading from hx711
+    GPIO.cleanup()
