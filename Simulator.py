d86123aeaa657f795fc4b844dd07974cfa8d3084
josueibecerra <47013134+josueibecerra@users.noreply.github.com>
2019-06-18 02:32:08 -0400

    no

---
+++ Simulator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO  # import GPIO
+from hx711 import HX711  # import the class HX711
+
+
+def weight_sensor():
+    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+    hx = HX711(dout_pin=21, pd_sck_pin=20)  # create an object
+    print(hx.get_raw_data_mean())  # get raw data reading from hx711
+    GPIO.cleanup()
+    return(hx.get_raw_data_mean)
