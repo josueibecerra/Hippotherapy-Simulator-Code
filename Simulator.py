a84a2592bee0c9fdaeb4a8cd78df892a445f32a0
josueibecerra <47013134+josueibecerra@users.noreply.github.com>
2019-06-19 00:02:02 -0400

    no

--- Simulator.py
+++ Simulator.py
@@ -6,15 +6,15 @@ from tkinter import *
 
 
 # set up weight sensors
-def weight_sensor():
+def measure_weight():
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
     hx = HX711(dout_pin=21, pd_sck_pin=20)  # create an object
-    print(hx.get_raw_data_mean())  # get raw data reading from hx711
+    hx.get_raw_data_mean()  # get raw data reading from hx711
     GPIO.cleanup()
     return int(hx.get_raw_data_mean())  # returns the measured weight when ran
 
 
-print(weight_sensor())  # NOT in final code; returns weight sensor value; code skips over def weight_sensor(): till now
+print(measure_weight())  # NOT in final code; returns weight sensor value; code skips over def weight_sensor(): till now
 
 
 # set up motors
