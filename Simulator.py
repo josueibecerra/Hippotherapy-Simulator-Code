d9d6e4489cc5e1e60b4de66d77981903e7e62ede
josueibecerra <47013134+josueibecerra@users.noreply.github.com>
2019-07-05 23:27:11 -0400

    gpio.cleanup() under finally

--- Simulator.py
+++ Simulator.py
@@ -94,8 +94,8 @@ root = tk.Tk()
 
 root.geometry('480x320')
 
-start_button = tk.Button(root, text='START', bg='grey', command=start_motors)
-stop_button = tk.Button(root, text='STOP', bg='red', command=stop_motors)
+start_button = tk.Button(root, text='START', bg='grey', command=start_motors, font=('Times', 40))
+stop_button = tk.Button(root, text='STOP', bg='red', command=stop_motors, font=('Times', 40))
 start_button.pack(side='top', expand=True, fill='both')
 stop_button.pack(side='bottom', expand=True, fill='both')
 
