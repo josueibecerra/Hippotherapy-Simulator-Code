87b81685887e47d92bd3955d75dcfb020a2efa52
josueibecerra <47013134+josueibecerra@users.noreply.github.com>
2019-07-21 14:28:52 -0400

    gpio.cleanup() under finally

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-try:
-    print('test1')
-except AttributeError:
-    print('test2')
-else:
-    print('test3')
+import tkinter as tk
+import time
+
+
+def start_motors():
+    while True:
+        print('motors running')
+        time.sleep(0.1)
+
+
+def stop_motors():
+    while True:
+        print('motors stopped')
+        time.sleep(0.1)
+
+
+# Start of GUI
+root = tk.Tk()
+
+root.geometry('480x320')
+
+start_button = tk.Button(root, text='START', bg='grey', command=start_motors, font=('Times', 40))
+stop_button = tk.Button(root, text='STOP', bg='red', command=stop_motors, font=('Times', 40))
+start_button.pack(side='top', expand=True, fill='both')
+stop_button.pack(side='bottom', expand=True, fill='both')
+
+root.mainloop()
